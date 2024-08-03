af26b104fa9c724accab8e25638f691267580c85
meet2aman <meet2amankushwaha@gmail.com>
2024-08-03 23:30:44 +0530

    integrating lexical editor

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import ShinyButton from "@/components/magicui/shiny-button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ShinyButton text={"Welcome"} className="" />
+    </main>
+  );
+}
